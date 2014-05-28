0f6cdf7446def84d71647f325f23226f91ce2460
Neqoxec <kyle@1970jan1-epo.ch>
2014-05-28 12:38:47 -0700

    x11/dzen2: add, change to git version

---
+++ x11/dzen2/files/patch-config.mk
@@ -0,0 +1,9 @@
+--- config.mk	2012-12-04 08:34:04.000000000 -0800
++++ config.mk.new	2014-04-27 08:51:01.723170546 -0700
+@@ -69,5 +69,5 @@
+ #LDFLAGS = ${LIBS}
+ 
+ # compiler and linker
+-CC = gcc
++CC = cc
+ LD = ${CC}
